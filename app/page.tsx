3c7c81a994e3306ea44a49716fb1795dda4544fe
fikalalif <fikalgans26@gmail.com>
2025-08-14 14:13:48 +0700

    setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center text-center text-2xl m-14 bg-fuchsia-500 text-white">
+      <h1>homepage</h1>
+    </div>
+  );
+}
