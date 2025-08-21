60b258fdf7ab785d1df05c5606bb4eccc0c64339
fikalalif <fikalgans26@gmail.com>
2025-08-21 19:41:15 +0700

    add: hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center text-center text-2xl m-14 bg-fuchsia-500 text-white">
-        <h1>homepage</h1>
+      <div>
+        <Hero/>
       </div>
     </>
   );
