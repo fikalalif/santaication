68ab8d990c39651d2d006e1affdcbee9a3bc3684
fikalalif <fikalgans26@gmail.com>
2025-08-22 20:02:29 +0700

    add: card

--- components/hero.tsx
+++ components/hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
             Book Now
           </Link>
           <Link
-            href="/room"
+            href="/contact"
             className="bg-transparent border border-orange-400 text-white hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold
             hover:scale-105 hover:shadow-lg"
           >
