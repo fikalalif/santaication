c85a95b51621d23fe0654df0e804416a4af403fe
fikalalif <fikalgans26@gmail.com>
2025-08-23 19:16:44 +0700

    update: card

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,15 @@ export default function Home() {
     <>
       <div>
         <Hero/>
-        <Main/>
+        <div className="mt-16">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+            <p className="py-3">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, sapiente.
+            </p>
+          </div>
+          <Main/>
+        </div>
       </div>
     </>
   );
