ff343ef8e52a82b76e24657dc5fa51b1241f4dd9
fikalalif <fikalgans26@gmail.com>
2025-08-27 23:55:51 +0700

    add: auth login google

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,10 +1,18 @@
 import { FaG } from "react-icons/fa6";
+import { signIn } from "@/auth";
 
 export const LoginGoogleButton = () => {
   return (
-    <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-600 cursor-pointer">
-      <FaG className="size-6"/>
-      Sign in With Google
-    </button>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-600 cursor-pointer">
+        <FaG className="size-6" />
+        Sign in With Google
+      </button>
+    </form>
   );
 };
