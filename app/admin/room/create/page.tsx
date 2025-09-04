8deb94a2b7453ba09196954bcd519c1487f44d9e
fikalalif <fikalgans26@gmail.com>
2025-09-04 20:32:25 +0700

    add: create room

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -1,10 +1,11 @@
-import CreateFrom from "@/components/admin/room/create-form"
+import CreateRoom from "@/components/admin/room/create-room"
+
 
 
 const CreateRoomPage = () => {
   return (
     <div className='max-w-screen-xl px-4 py-16 mt-20 mx-auto'>
-        <CreateFrom/>
+        <CreateRoom/>
     </div>
   )
 }
---
+++ components/admin/room/create-room.tsx
@@ -0,0 +1,12 @@
+import CreateFrom from "./create-form"
+
+const CreateRoom = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Create New Room</h1>
+        <CreateFrom/>
+    </div>
+  )
+}
+
+export default CreateRoom
