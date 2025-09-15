c1dc14253647c674838d4bd0c75dbe7e4640b01b
fikalalif <fikalgans26@gmail.com>
2025-09-15 20:59:55 +0700

    success: update room

--- lib/action.ts
+++ lib/action.ts
@@ -7,7 +7,7 @@ import { del } from "@vercel/blob";
 import { revalidatePath } from "next/cache";
 
 export const saveRoom = async (image: string, prevState: unknown, formData: FormData) => {
-    if(!image) return {message : "Image is Required"}
+    if (!image) return { message: "Image is Required" }
 
     const rawData = {
         name: formData.get("name"),
@@ -18,23 +18,23 @@ export const saveRoom = async (image: string, prevState: unknown, formData: Form
     }
 
     const validatedFields = RoomSchema.safeParse(rawData);
-    if(!validatedFields.success){
-        return {error: validatedFields.error.flatten().fieldErrors}
+    if (!validatedFields.success) {
+        return { error: validatedFields.error.flatten().fieldErrors }
     }
 
-    const {name, description, price, capacity, amenities} = validatedFields.data;
+    const { name, description, price, capacity, amenities } = validatedFields.data;
 
     try {
         await prisma.room.create({
-            data:{
+            data: {
                 name,
                 description,
                 price,
                 capacity,
                 image,
-                RoomAmenities:{
-                    createMany:{
-                        data: amenities.map((item)=>({
+                RoomAmenities: {
+                    createMany: {
+                        data: amenities.map((item) => ({
                             amenitiesId: item
                         }))
                     }
@@ -71,14 +71,62 @@ export const ContactMessage = async (prevState: unknown, formData: FormData) => 
 };
 
 //delete room
-export const deleteRoom = async(id: string, image:string) => { 
+export const deleteRoom = async (id: string, image: string) => {
     try {
         await del(image);
         await prisma.room.delete({
-            where:{id},
+            where: { id },
         });
     } catch (error) {
         console.log(error);
     }
     revalidatePath("/admin/room");
- };
+};
+
+export const updateRoom = async (image: string, roomId: string, prevState: unknown, formData: FormData) => {
+    if (!image) return { message: "Image is Required" }
+
+    const rawData = {
+        name: formData.get("name"),
+        description: formData.get("description"),
+        capacity: formData.get("capacity"),
+        price: formData.get("price"),
+        amenities: formData.getAll("amenities")
+    }
+
+    const validatedFields = RoomSchema.safeParse(rawData);
+    if (!validatedFields.success) {
+        return { error: validatedFields.error.flatten().fieldErrors }
+    }
+
+    const { name, description, price, capacity, amenities } = validatedFields.data;
+
+    try {
+        await prisma.$transaction([
+            prisma.room.update({
+                where: { id: roomId },
+                data: {
+                    name,
+                    description,
+                    image,
+                    price,
+                    capacity,
+                    RoomAmenities: {
+                        deleteMany: {}
+                    }
+                }
+            }),
+            prisma.roomAmenities.createMany({
+                data: amenities.map((item) => ({
+                    roomId,
+                    amenitiesId: item
+                }))
+            })
+        ])
+    }
+    catch (error) {
+        console.log(error);
+    }
+    revalidatePath("/admin/room");
+    redirect("/admin/room");
+}
